7a9fde22568776f50b212241f2716798c3cd05a9
johardi <josef.hardi@stanford.edu>
2017-03-03 18:42:31 -0800

    Remove hard-coded JSON document as program input

--- src/test/java/org/metadatacenter/model/validation/MetadataInstanceTest.java
+++ src/test/java/org/metadatacenter/model/validation/MetadataInstanceTest.java
@@ -3,17 +3,19 @@ package org.metadatacenter.model.validation;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.io.File;
+import java.io.InputStream;
 
 public class MetadataInstanceTest {
 
   public static void main(String[] args) throws Exception {
-//    String filename = args[0];
+    String filename = args[0];
+
+    InputStream in = MetadataInstanceTest.class.getClassLoader().getResourceAsStream(filename);
 
     ObjectMapper mapper = new ObjectMapper();
-    JsonNode root = mapper.readTree(new File("/Users/jhardi/Documents/Projects/CEDAR/cedar-model-validation-library/target/test-classes/metadata-example.txt"));
+    JsonNode input = mapper.readTree(in);
 
-    JsonNode output = new MetadataInstance(root).asJson();
+    JsonNode output = new MetadataInstance(input).asJson();
     String prettyPrint = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(output);
 
     System.out.println(prettyPrint);
