3465b3aba4a4ef70782a35e0af9925acc0f10d07
johardi <josef.hardi@stanford.edu>
2017-03-09 09:57:57 -0800

    Use option to select the serialization format to output the instance

--- src/test/java/org/metadatacenter/model/validation/MetadataInstanceTest.java
+++ src/test/java/org/metadatacenter/model/validation/MetadataInstanceTest.java
@@ -1,21 +1,34 @@
 package org.metadatacenter.model.validation;
 
+import com.github.jsonldjava.core.JsonLdError;
 import org.apache.commons.io.IOUtils;
 
+import java.io.IOException;
 import java.io.InputStream;
 
 public class MetadataInstanceTest {
 
   public static void main(String[] args) throws Exception {
-    String filename = args[0];
+    String option = args[0];
+    String filename = args[1];
 
     InputStream in = MetadataInstanceTest.class.getClassLoader().getResourceAsStream(filename);
     String input = IOUtils.toString(in, "UTF-8");
 
-    String jsonPrint = new TemplateInstance(input).asJson();
-    System.out.println(jsonPrint);
+    if ("-json".equals(option)) {
+      printJson(input);
+    } else if ("-rdf".equals(option)) {
+      printRdf(input);
+    }
+  }
 
+  private static void printRdf(String input) throws JsonLdError, IOException {
     String rdfPrint = new TemplateInstance(input).asRdf();
     System.out.println(rdfPrint);
+  }
+
+  private static void printJson(String input) throws IOException {
+    String jsonPrint = new TemplateInstance(input).asJson();
+    System.out.println(jsonPrint);
   }
 }
