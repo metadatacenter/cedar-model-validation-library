9164ae35c3aa79b040480dc0845e7dacf6b34529
johardi <josef.hardi@stanford.edu>
2017-04-21 10:41:10 -0700

    Changes due to applying the new ModelValidator framework

--- src/main/java/org/metadatacenter/model/validation/ValidateTemplateElement.java
+++ src/main/java/org/metadatacenter/model/validation/ValidateTemplateElement.java
@@ -3,31 +3,28 @@ package org.metadatacenter.model.validation;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.fge.jackson.JacksonUtils;
-import com.github.fge.jsonschema.core.report.LogLevel;
-import com.github.fge.jsonschema.core.report.ProcessingMessage;
-import com.github.fge.jsonschema.core.report.ProcessingReport;
+import org.metadatacenter.model.validation.report.ErrorItem;
+import org.metadatacenter.model.validation.report.ValidationReport;
 
 import java.io.File;
 import java.io.IOException;
 
-public class ValidateTemplateElement
-{
+public class ValidateTemplateElement {
   private static final ObjectMapper MAPPER = JacksonUtils.newMapper();
 
-  public static void main(String[] args)
-  {
-    if (args.length != 1)
+  public static void main(String[] args) {
+    if (args.length != 1) {
       Usage();
+    }
 
     try {
       File templateElementFile = new File(args[0]);
       JsonNode templateElementNode = MAPPER.readTree(templateElementFile);
 
       CEDARModelValidator cedarModelValidator = new CEDARModelValidator();
-      ProcessingReport processingReport = cedarModelValidator.validateTemplateElementNode(templateElementNode);
-      for (ProcessingMessage processingMessage : processingReport) {
-        processingMessage.setLogLevel(LogLevel.DEBUG);
-        System.out.println("Message: " + processingMessage.getMessage());
+      ValidationReport validationReport = cedarModelValidator.validateTemplateElement(templateElementNode);
+      for (ErrorItem errorItem : validationReport.getErrors()) {
+        System.out.println("Message: " + errorItem.getMessage());
       }
     } catch (IOException e) {
       System.err.println("IO exception: " + e.getMessage());
@@ -36,8 +33,7 @@ public class ValidateTemplateElement
     }
   }
 
-  private static void Usage()
-  {
+  private static void Usage() {
     System.err.println("Usage: " + ValidateTemplateElement.class.getName() + " <templateElementFileName>");
     System.exit(1);
   }
--- src/main/java/org/metadatacenter/model/validation/ValidateTemplateField.java
+++ src/main/java/org/metadatacenter/model/validation/ValidateTemplateField.java
@@ -4,32 +4,29 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.fge.jackson.JacksonUtils;
 import com.github.fge.jsonschema.core.exceptions.ProcessingException;
-import com.github.fge.jsonschema.core.report.LogLevel;
-import com.github.fge.jsonschema.core.report.ProcessingMessage;
-import com.github.fge.jsonschema.core.report.ProcessingReport;
+import org.metadatacenter.model.validation.report.ErrorItem;
+import org.metadatacenter.model.validation.report.ValidationReport;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 
-public class ValidateTemplateField
-{
+public class ValidateTemplateField {
   private static final ObjectMapper MAPPER = JacksonUtils.newMapper();
 
-  public static void main(String[] args) throws ProcessingException, IOException, URISyntaxException
-  {
-    if (args.length != 1)
+  public static void main(String[] args) throws ProcessingException, IOException, URISyntaxException {
+    if (args.length != 1) {
       Usage();
+    }
 
     try {
       File templateFieldFile = new File(args[0]);
       JsonNode templateFieldNode = MAPPER.readTree(templateFieldFile);
 
       CEDARModelValidator cedarModelValidator = new CEDARModelValidator();
-      ProcessingReport processingReport = cedarModelValidator.validateTemplateFieldNode(templateFieldNode);
-      for (ProcessingMessage processingMessage : processingReport) {
-        processingMessage.setLogLevel(LogLevel.DEBUG);
-        System.out.println("Message: " + processingMessage.getMessage());
+      ValidationReport validationReport = cedarModelValidator.validateTemplateField(templateFieldNode);
+      for (ErrorItem errorItem : validationReport.getErrors()) {
+        System.out.println("Message: " + errorItem.getMessage());
       }
     } catch (ProcessingException e) {
       System.err.println("Processing exception: " + e.getMessage());
@@ -42,8 +39,7 @@ public class ValidateTemplateField
     }
   }
 
-  private static void Usage()
-  {
+  private static void Usage() {
     System.err.println("Usage: " + ValidateTemplateField.class.getName() + " <templateFieldFileName>");
     System.exit(1);
   }
