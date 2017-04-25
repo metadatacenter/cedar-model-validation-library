eca2e289dbf9552881b4452fa92a6e822ed49d76
johardi <josef.hardi@stanford.edu>
2017-04-25 10:25:07 -0700

    Replace SchemaUrls with CedarConstants

--- src/main/java/org/metadatacenter/model/core/CedarConstants.java
+++ src/main/java/org/metadatacenter/model/core/CedarConstants.java
@@ -2,6 +2,8 @@ package org.metadatacenter.model.core;
 
 public class CedarConstants {
 
+  public static final String TEMPLATE_TYPE_URI = "https://schema.metadatacenter.org/core/Template";
+
   public static final String TEMPLATE_ELEMENT_TYPE_URI = "https://schema.metadatacenter.org/core/TemplateElement";
 
   public static final String TEMPLATE_FIELD_TYPE_URI = "https://schema.metadatacenter.org/core/TemplateField";
