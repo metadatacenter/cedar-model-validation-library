8ac7af7572e78d67c6f845ab82e31b8c265a1607
johardi <josef.hardi@stanford.edu>
2017-05-11 17:02:49 -0700

    Update the run script for test the validation library

---
+++ scripts/validate-template.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Validate a CEDAR template
+#
+# e.g., 
+#
+# validate-template <template_file_path>
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: <template_file_path>"
+    exit 1
+fi
+
+template_file_path=$1
+
+BASE_DIR="`dirname \"$0\"`"              # relative
+BASE_DIR="`( cd \"$BASE_DIR\" && pwd )`"  # absolutized and normalized
+PROJECT_BASE_DIR="`( cd .. && pwd )`"  # absolutized and normalized
+if [ -z "$PROJECT_BASE_DIR" ] ; then
+  # error; for some reason, the path is not accessible
+  # to the script (e.g. permissions re-evaled after suid)
+  exit 1  # fail
+fi
+
+SCRIPTS_DIR=${PROJECT_BASE_DIR}/scripts
+RESOURCES_DIR=${PROJECT_BASE_DIR}/src/main/resources
+
+echo "Validating template" ${template_file_path} "using Python validator"
+
+python ${SCRIPTS_DIR}/jsvalid.py -s ${RESOURCES_DIR}/template-schema.json -i ${template_file_path}
+
