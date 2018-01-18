522a7b11fb83698af21f8b0f82d40e4b31f8eceb
johardi <josef.hardi@stanford.edu>
2018-01-18 11:59:43 -0800

    Make the required field as a non-mandatory field
    re: #68

--- src/test/java/org/metadatacenter/model/validation/TemplateFieldValidationTest.java
+++ src/test/java/org/metadatacenter/model/validation/TemplateFieldValidationTest.java
@@ -3,6 +3,7 @@ package org.metadatacenter.model.validation;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.metadatacenter.model.validation.report.ValidationReport;
 
@@ -299,6 +300,7 @@ public class TemplateFieldValidationTest extends BaseValidationTest {
   }
 
   @Test
+  @Ignore
   public void shouldFailMissingRequired() {
     // Arrange
     String fieldString = TestResourcesUtils.getStringContent("fields/text-field.json");
