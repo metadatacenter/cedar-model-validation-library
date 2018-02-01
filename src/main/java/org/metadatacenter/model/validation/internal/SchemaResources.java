3eb5673682de2e356796cc368f1e9e4d8d07e89d
johardi <josef.hardi@stanford.edu>
2018-02-01 11:29:11 -0800

    Code tidy

--- src/main/java/org/metadatacenter/model/validation/internal/SchemaResources.java
+++ src/main/java/org/metadatacenter/model/validation/internal/SchemaResources.java
@@ -4,8 +4,8 @@ public class SchemaResources {
 
   public static final String TEMPLATE_SCHEMA = "template-schema.json";
   public static final String ELEMENT_SCHEMA = "element-schema.json";
-  public static final String SINGLE_DATA_FIELD_SCHEMA = "single-data-field-schema.json";
-  public static final String SINGLE_OBJECT_FIELD_SCHEMA = "single-object-field-schema.json";
-  public static final String MULTI_DATA_FIELD_SCHEMA = "multi-data-field-schema.json";
+  public static final String LITERAL_FIELD_SCHEMA = "single-data-field-schema.json";
+  public static final String OBJECT_FIELD_SCHEMA = "single-object-field-schema.json";
+  public static final String MULTIENTRIES_FIELD_SCHEMA = "multi-data-field-schema.json";
   public static final String STATIC_FIELD_SCHEMA = "static-field-schema.json";
 }
