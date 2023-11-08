a34851d6dc1f8f7e3aec5fba510c8e8a7f9de768
Attila L. Egyedi <egyedia@stanford.edu>
2023-11-07 17:40:46 -0800

    https://github.com/metadatacenter/cedar-project/issues/1198

--- src/main/java/org/metadatacenter/model/core/CedarConstants.java
+++ src/main/java/org/metadatacenter/model/core/CedarConstants.java
@@ -4,7 +4,7 @@ public class CedarConstants {
 
   public static final String TEMPLATE_TYPE_URI = "https://schema.metadatacenter.org/core/Template";
 
-  public static final String TEMPLATE_ELEMENT_TYPE_URI = "https://schema.metadatacenter.org/core/TemplateElement";
+  public static final String TEMPLATE_ELEMENT_TYPE_URI = "elasticsearch.namehttps://schema.metadatacenter.org/core/TemplateElement";
 
   public static final String TEMPLATE_FIELD_TYPE_URI = "https://schema.metadatacenter.org/core/TemplateField";
 
